0679d2fbb42bed0de337e7f28d9748153b5de7d2
DavidManasian <126864606+DavidManasian@users.noreply.github.com>
2023-03-14 15:25:48 +0300

    Example

---
+++ lesson6_step16.py
@@ -0,0 +1,29 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time 
+import math
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+link = "http://suninjuly.github.io/redirect_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    button = browser.find_element(By.XPATH,"//button[@class='trollface btn btn-primary']")
+    button.click()
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(browser.window_handles[1])
+    x_element = browser.find_element(By.CSS_SELECTOR, "[id='input_value']")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element(By.CSS_SELECTOR, "[id='answer']")
+    input1.send_keys(y)
+    button = browser.find_element(By.XPATH,"//button[@class='btn btn-primary']")
+    button.click()
+finally:
+    # успеваем скопировать код за 30 секунд
+    time.sleep(30)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
